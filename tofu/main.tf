f85479dbf4595522f9d27fc91e2c471dc992a920
Vitor <amazon.unlikableness@simplelogin.co>
2025-05-17 14:58:58 -0400

    bootstrap argo

--- tofu/main.tf
+++ tofu/main.tf
@@ -91,3 +91,16 @@ module "argocd" {
 #     }
 #   }
 # }
+
+resource "helm_release" "argocd" {
+  name = "argocd"
+  repository       = "https://argoproj.github.io/argo-helm"
+  chart            = "argo-cd"
+  namespace        = "argocd"
+  version          = "8.0.3"
+  create_namespace = true
+
+  values = [
+    file("../argocd/application.yaml")
+  ]
+}
